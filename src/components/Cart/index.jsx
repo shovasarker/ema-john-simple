9bf6bff86e4b64412e3f1b1a8dd2b4ae9d5f147c
ShovaSarker <shovasarker01@gmail.com>
2022-03-23 01:10:08 +0600

    Cart Component code Refactored

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -2,34 +2,23 @@ import React from 'react'
 import './cart.css'
 
 const Cart = ({ cart }) => {
+  const totalPrice = cart?.reduce(
+    (totalPrice, product) => (totalPrice += product.price),
+    0
+  )
+  const totalShipping = cart?.reduce(
+    (totalShipping, product) => (totalShipping += product.shipping),
+    0
+  )
+  const tax = parseFloat((totalPrice * 0.1).toFixed(2))
   return (
     <div className='cart-container'>
-      <h4>Order Summary</h4>
+      <h4 className='cart-title'>Order Summary</h4>
       <p>Selected Items: {cart.length}</p>
-      <p>
-        Total Price: $
-        {cart?.reduce(
-          (totalPrice, product) => (totalPrice += product.price),
-          0
-        )}
-      </p>
+      <p>Total Price: ${totalPrice}</p>
 
-      <p>
-        Total Shipping Charge: $
-        {cart?.reduce(
-          (totalShipping, product) => (totalShipping += product.shipping),
-          0
-        )}
-      </p>
-      <p>
-        Tax: $
-        {Math.round(
-          cart?.reduce(
-            (totalPrice, product) => (totalPrice += product.price),
-            0
-          ) * 0.1
-        )}
-      </p>
+      <p>Total Shipping Charge: ${totalShipping}</p>
+      <p>Tax: ${tax}</p>
     </div>
   )
 }
