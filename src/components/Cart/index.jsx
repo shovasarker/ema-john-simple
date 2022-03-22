01d1da50d61f7c4642df21967d6099aca6421061
ShovaSarker <shovasarker01@gmail.com>
2022-03-23 02:22:49 +0600

    Some code added in Shop component to save cart data in local storage and changed some code for adding product in cart

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import './cart.css'
 
 const Cart = ({ cart }) => {
+  const totalQuantity = cart?.reduce(
+    (totalQuantity, product) => (totalQuantity += product.quantity),
+    0
+  )
   const totalPrice = cart?.reduce(
-    (totalPrice, product) => (totalPrice += product.price),
+    (totalPrice, product) => (totalPrice += product.price * product.quantity),
     0
   )
   const totalShipping = cart?.reduce(
@@ -11,14 +15,18 @@ const Cart = ({ cart }) => {
     0
   )
   const tax = parseFloat((totalPrice * 0.1).toFixed(2))
+  const grandTotal = totalPrice + totalShipping + tax
   return (
     <div className='cart-container'>
       <h4 className='cart-title'>Order Summary</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {totalQuantity}</p>
       <p>Total Price: ${totalPrice}</p>
 
       <p>Total Shipping Charge: ${totalShipping}</p>
       <p>Tax: ${tax}</p>
+      <h4 className='cart-grand-total'>
+        Grand Total: ${grandTotal.toFixed(2)}
+      </h4>
     </div>
   )
 }
--- src/components/Shop/index.jsx
+++ src/components/Shop/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { addToDb, getShoppingdCart } from '../../utilities/fakedb'
 import Cart from '../Cart'
 import Product from '../Product'
 import './shop.css'
@@ -17,10 +18,32 @@ const Shop = () => {
   }, [])
 
   useEffect(() => {
-    console.log(cart)
-  }, [cart])
+    if (!products.length > 0) return
+    const storedCart = getShoppingdCart()
+    const cartProducts = Object.entries(storedCart)?.map(([id, quantity]) => {
+      return {
+        ...products.find((product) => product.id === id),
+        quantity: quantity,
+      }
+    })
+    setCart(cartProducts)
+    console.log(cartProducts)
+  }, [products])
+
   const handleAddToCart = (product) => {
-    setCart([...cart, product])
+    const findProduct = cart?.find(({ id }) => product.id === id)
+    if (findProduct) {
+      setCart(
+        cart?.map((item) =>
+          product.id === item.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      )
+    } else {
+      setCart([...cart, { ...product, quantity: 1 }])
+    }
+    addToDb(product.id)
   }
 
   return (
